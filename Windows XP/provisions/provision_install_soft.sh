0882d01a45747675dcce424f9a93cef1755edf96
PostUlatKojun <PostUlatKojun@gmail.com>
2017-06-20 21:49:54 +0300

    add Windows XP

---
+++ Windows XP/provisions/provision_install_soft.sh
@@ -0,0 +1,7 @@
+choco list -l
+choco update -y all
+choco install -y python2 --ignore-checksums
+choco install -y firefoxesr --ignore-checksums
+choco install -y teamviewer.host --ignore-checksums
+#choco install -y TotalCommander --ignore-checksums
+#choco install -y git.install
