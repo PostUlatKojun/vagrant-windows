6e73139878f8e2e1ace117eb98a620b8850f8d2d
PostUlatKojun <PostUlatKojun@gmail.com>
2017-08-07 20:52:06 +0300

    update Vista provisions

--- Windows Vista/provisions/provision_install_soft.ps1
+++ Windows Vista/provisions/provision_install_soft.ps1
@@ -1,13 +1,16 @@
 choco list -l
 choco update -y all
 # mintty -D --windows hide apt-cyg install nano
-choco install -y python2 git.install teamviewer.host firefoxesr tortoisegit skype notepadplusplus.install sublimetext3
+choco install -y python2 git.install firefoxesr skype sublimetext3
+choco install -y teamviewer.host --ignore-checksums
+choco install -y tortoisegit --version 2.4.0.2
+choco install -y notepadplusplus.install 7.3.3
 # python2 --version 2.7.13
-# git.install --version 2.13.0
+# git.install --version 2.14.0.2
 # teamviewer.host --version 12.0.78313
-# firefoxesr --version 52.1.2
+# firefoxesr --version 52.2.1
 # tortoisegit --version 2.4.0.2
-# skype --version 7.36.0.101
-# notepadplusplus.install --version 7.4.1
+# skype --version 7.37.0.103
+# notepadplusplus.install --version 7.3.3
 # sublimetext3 --version 3.0.0.3126
 choco install -y totalcommander --version 8.01
--- Windows Vista/provisions/provision_install_soft.sh
+++ Windows Vista/provisions/provision_install_soft.sh
@@ -1,3 +1,16 @@
 choco list -l
 choco update -y all
-choco install -y git.install
+# mintty -D --windows hide apt-cyg install nano
+choco install -y python2 git.install firefoxesr skype sublimetext3
+choco install -y teamviewer.host --ignore-checksums
+choco install -y tortoisegit --version 2.4.0.2
+choco install -y notepadplusplus.install 7.3.3
+# python2 --version 2.7.13
+# git.install --version 2.14.0.2
+# teamviewer.host --version 12.0.78313
+# firefoxesr --version 52.2.1
+# tortoisegit --version 2.4.0.2
+# skype --version 7.37.0.103
+# notepadplusplus.install --version 7.3.3
+# sublimetext3 --version 3.0.0.3126
+choco install -y totalcommander --version 8.01
